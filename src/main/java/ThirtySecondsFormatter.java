d82723b25ec6ca6db5853bffa9c25ca017b71736
loisgh <loisgh@nyc.rr.com>
2016-09-19 11:11:14 -0400

    first commit

--- src/main/java/ThirtySecondsFormatter.java
+++ src/main/java/ThirtySecondsFormatter.java
@@ -4,10 +4,12 @@
 public class ThirtySecondsFormatter {
 
     public static String format(double quote) {
+
         //(int)quote            -- returns the integer portion of the input
         //quote - (int)quote    -- returns the decimal portion
         //multiply the decimal portion of the quote by 32 to get the number of 32'nds in the quote
         //format the decimal portion so that leading zeros are included if the result is only one position
         return (int)quote + "-" + String.format("%02d",(int)(32 * (quote - (int)quote)));
+        
     }
 }
--- src/test/java/ThirtySecondsFormatterTest.java
+++ src/test/java/ThirtySecondsFormatterTest.java
@@ -8,6 +8,7 @@ public class ThirtySecondsFormatterTest {
 
     @Test
     public void FormatQuoteTest(){
+
         Assert.assertEquals(ThirtySecondsFormatter.format(23.00d), "23-00");
         Assert.assertEquals(ThirtySecondsFormatter.format(23.03125d), "23-01");
         Assert.assertEquals(ThirtySecondsFormatter.format(99.0625d),"99-02");
@@ -15,5 +16,6 @@ public class ThirtySecondsFormatterTest {
         Assert.assertEquals(ThirtySecondsFormatter.format(23.25d), "23-08");
         Assert.assertEquals(ThirtySecondsFormatter.format(23.50d), "23-16");
         Assert.assertEquals(ThirtySecondsFormatter.format(23.75d), "23-24");
+
     }
 }
